eb609537262afeeb6f840d925c628a2bad2e0d27
Artem Alekseev <lexx73652@gmail.com>
2025-02-04 12:44:47 +0400

    Added check errors and edit config

--- config/yandex-translate.php
+++ config/yandex-translate.php
@@ -2,4 +2,5 @@
 
 return [
     'api-key' => env('YANDEX_TRANSLATE_API_KEY'),
+    'api-url' => env('YANDEX_TRANSLATE_API_URL', 'https://translate.api.cloud.yandex.net/translate/v2/translate'),
 ];
--- src/Translate.php
+++ src/Translate.php
@@ -2,12 +2,15 @@
 
 namespace YandexTranslate;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\Response;
 
 class Translate
 {
     private string $apiKey;
+    private string $apiUrl;
     private LanguageEnum $sourceLanguage = LanguageEnum::RUSSIAN;
     private LanguageEnum $targetLanguage = LanguageEnum::ENGLISH;
     private FormatEnum $format = FormatEnum::FORMAT_UNSPECIFIED;
@@ -19,6 +22,7 @@ class Translate
     public function __construct()
     {
         $this->apiKey = config('yandex-translate.api-key');
+        $this->apiUrl = config('yandex-translate.api-url');
     }
 
     public function setSourceLanguage(LanguageEnum $languageEnum): self
@@ -56,26 +60,27 @@ class Translate
         return $this;
     }
 
-    public function translate()
+    public function translate(): Collection
     {
-        dd($this->texts);
+        if ($this->texts->isEmpty()) {
+            throw new \Exception('Empty text, use addText function', 500);
+        }
 
         $response = Http::withHeaders(['Authorization' => 'Api-Key ' . $this->apiKey])
-            ->post('https://translate.api.cloud.yandex.net/translate/v2/translate', [
+            ->post($this->apiUrl, [
                 "sourceLanguageCode" => $this->sourceLanguage->value,
                 "targetLanguageCode" => $this->targetLanguage->value,
                 "format" => $this->format->value,
-                "texts" => $this->texts->toArray(),
+                "texts" => $this->texts->values()->toArray(),
                 "folderId" => $this->folderId,
                 "speller" => $this->speller,
             ]);
 
-        $trans = [];
+        $response->onError(fn(Response $response) => throw new \Exception($response->body(), $response->status()));
+        $translations = $response->object()->translations;
+        $texts = $this->texts->keys()
+            ->mapWithKeys(fn($key, $value) => [$key => Arr::get($translations, $value)?->text]);
 
-        foreach ($response->translations as $key => $value) {
-            $trans[$keys[$key]] = $value->text;
-        }
-
-        return $trans;
+        return $texts;
     }
 }
